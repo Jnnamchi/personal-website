46643220f415a5447fdebfefc5e707eaf50e2a28
Jnnamchi <john.nnamchi@mail.mcgill.ca>
2024-09-12 18:39:43 -0700

    Added vite custom build steps

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/crapsgame/crapsgame.js',
+          dest: './src/crapsgame/'
+        },
+        {
+          src: './src/randomQuoteGenerator/randomQuoteGenerator.js',
+          dest: './src/randomQuoteGenerator/'
+        },
+        {
+          src: './src/aboutmesection/John_Nnamchi_CV.pdf',
+          dest: './src/aboutmesection/'
+        },
+      ],
+    }),
+  ],
 })
